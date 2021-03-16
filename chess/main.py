1e483f77fa72901c2be8bf2117727d0201580eaf
Jeff <linjeff16@gmail.com>
2021-03-16 14:52:19 -0400

    added graphic interface of the board

--- chess/main.py
+++ chess/main.py
@@ -1,9 +1,12 @@
 import pygame as p
-import chess
+import engine
+
 WIDTH = HEIGHT = 512
 DIMENSION = 8
 SQUARESIZE = WIDTH//DIMENSION
 IMAGES = {} #dictionary of images
+FPS = 15
+p.init()
 """
 because loading images everytime can be expensive, use dictionary to load them in all at first
 """
@@ -12,5 +15,48 @@ def loadImages():
     pieces = ["wP","wR","wN","wB","wQ","wK","wB","wN","wR","bR","bN","bB","bQ","bK","bB","bN","bR","bP"]
     for piece in pieces:
         #u can iterate each things without using pieces[1] or pieces[2] cause python is sickkkkkkkkkk
-        IMAGES[piece] = p.image.load("images/"+piece+".png")
+        IMAGES[piece] = p.transform.scale(p.image.load("images/"+piece+".png"),(SQUARESIZE-3,SQUARESIZE-3))
     #now all images are saved inside, so we can access it like IMAGES["wP"] or IMAGES["bN"]
+
+def main():
+    screen = p.display.set_mode((WIDTH,HEIGHT)) #takes in a tuple and sets a square/rectangle with the given size
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = engine.gameState()
+    print(gs.board)
+    loadImages()
+    running = True
+    while running:
+        for event in p.event.get():
+            if event.type == p.QUIT:
+                running = False
+        drawGameState(screen, gs)
+        clock.tick(FPS)
+        p.display.flip()
+
+
+#graphics of the game
+def drawGameState(screen, gs):
+    drawBoard(screen) #draw the entire board with squares
+    drawPieces(screen, gs) #draw the pieces on top of the square
+
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("light gray")]
+    for row in range(DIMENSION):
+        for column in range(DIMENSION):
+            #(row+column)%2 gives u whether its a white space or not
+            squareColor = colors[((row+column)%2)]
+            p.draw.rect(screen, squareColor, p.Rect(column*SQUARESIZE,row*SQUARESIZE,SQUARESIZE,SQUARESIZE))
+
+
+def drawPieces(screen, gs):
+    for row in range(DIMENSION):
+        for column in range(DIMENSION):
+            piece = gs.board[row][column];
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(column*SQUARESIZE,row*SQUARESIZE,SQUARESIZE,SQUARESIZE))
+
+
+if __name__ == "__main__":
+    main()
